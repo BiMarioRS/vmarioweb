e13fb5a6a15beea2377b3046aba7a9d8a60358e0
Dani Mas <danimascarre@gmail.com>
2022-03-02 14:34:30 +0100

    dark-light

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
 import Content from "./Fairlaunch/Content/Content";
-import useLocalStorage from "use-local-storage";
 import Toggle from "./Fairlaunch/components/Toggle/Toggle";
+import { useState } from "react";
 
 function App() {
-  const defaultDark = window.matchMedia(
-    "(prefers-color-scheme: light)"
-  ).matches;
-  const [theme, setTheme] = useLocalStorage(
-    "theme",
-    defaultDark ? "light" : "dark"
-  );
+  const defaultLight = "(prefers-color-scheme: light)"
+
+  const [theme, setTheme] = useState(defaultLight ? "light" : "dark");
 
   const switchTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
