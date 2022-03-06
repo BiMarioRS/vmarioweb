e4e43c00c53ac559e450f870d22c2631ca1cc33d
Dani Mas <danimascarre@gmail.com>
2022-03-06 23:31:33 +0100

    working with digimons names

--- server/index.js
+++ server/index.js
@@ -15,12 +15,36 @@ app.use((req, res, next) => {
   next()
 });
 
-app.get("/api/hello", (req, res, next) =>{
+let digimonNames = "";
 
-  res.send({express: "aqui van los subs"})
+   // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
+   puppeteer.launch().then(async function(browser) {
+    const page = await browser.newPage();
+    await page.goto('http://digidb.io/digimon-list/');
+
+    // Targeting the DOM Nodes that contain the Digimon names
+    digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function(digimons) {
+    // Mapping each Digimon name to an array
+        return digimons.map(function(digimon) {
+      return digimon.innerText;
+    });
+  });
+
+    // Log the array of Digimon names to the terminal
+    console.log(digimonNames);
+
+    // Closing the Puppeteer controlled headless browser
+    await browser.close();
+});
+
+
+app.get("/api/hello", (req, res, next) =>{
+  res.send({express: digimonNames})
   next()
 
 })
+
+
 
 
 // start express server on port 5000
