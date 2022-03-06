17bd83799424bc8e5cc9bf5b93d1d72c9b16dabe
Dani Mas <danimascarre@gmail.com>
2022-03-07 00:07:21 +0100

    scraper

--- server/index.js
+++ server/index.js
@@ -15,23 +15,24 @@ app.use((req, res, next) => {
   next()
 });
 
-let digimonNames = "";
+let subs = "";
 
-   // Launching the Puppeteer controlled headless browser and navigate to the Digimon website
+   // Launching the Puppeteer controlled headless browser and navigate to the subs website
    puppeteer.launch().then(async function(browser) {
     const page = await browser.newPage();
-    await page.goto('http://digidb.io/digimon-list/');
+    await page.goto('https://socialcounts.org/youtube-live-subscriber-count/UC106ODNmI-M5BDMx5_2yNMQ');
 
-    // Targeting the DOM Nodes that contain the Digimon names
-    digimonNames = await page.$$eval('#digiList tbody tr td:nth-child(2) a', function(digimons) {
-    // Mapping each Digimon name to an array
-        return digimons.map(function(digimon) {
-      return digimon.innerText;
+
+    // Targeting the DOM Nodes that contain the subs names
+    subs = await page.$$eval('#count_value', function(subss) {
+    // Mapping each subs name to an array
+        return subss.map(function(subs) {
+      return subs.innerText;
     });
   });
 
-    // Log the array of Digimon names to the terminal
-    console.log(digimonNames);
+    // Log the array of subs names to the terminal
+    console.log(subs);
 
     // Closing the Puppeteer controlled headless browser
     await browser.close();
@@ -39,7 +40,7 @@ let digimonNames = "";
 
 
 app.get("/api/hello", (req, res, next) =>{
-  res.send({express: digimonNames})
+  res.send({express: subs[0]})
   next()
 
 })
