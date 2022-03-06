a37265b6f35b7b3277402afece23126d3e2f0744
Dani Mas <danimascarre@gmail.com>
2022-03-06 21:22:52 +0100

    api call

--- server/index.js
+++ server/index.js
@@ -4,26 +4,24 @@ const puppeteer = require("puppeteer")
 
 const app = express(); // create express app
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 // add middlewares
 app.use(express.static(path.join(__dirname, "..", "build")));
-app.use(express.json())
 
 app.use((req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  next()
 });
 
-app.get("/", (req, res) =>{
-  res.send("hola")
+app.get("/api/hello", (req, res, next) =>{
+
+  res.send({express: {data}})
+  next()
+
 })
 
-app.post("/", async (req, res) => {
-console.log("clicado")
-})
-
-/*  const browser = await puppeteer.launch({headless:false});
-  const page = await browser.newPage();
-  await page.goto('https://google.com');
-  await browser.close(); */
 
 // start express server on port 5000
 app.listen(5000, () => {
--- src/Fairlaunch/components/InfoVmario/InfoVmario.js
+++ src/Fairlaunch/components/InfoVmario/InfoVmario.js
@@ -1,47 +1,51 @@
 import { func } from "prop-types";
-import React from "react";
+import React, { Component } from "react";
 import "./Info-vmario.css";
 
 
-function InfoVmario() {
-  return (
-    <div className="info-vmario-content">
-      <div className="container-content-vmario">
-        <div className="name-heading">
-          <h1>vMario</h1>
-          <h1></h1>
-        </div>
-        <div className="subs-count">
-          <h4>Ya somos</h4>
-          <div className="subs-number">
-            <div className="num-container">
-              <h2 className="yt-count">100000</h2>
-              <button onClick={requestData} className="button">get data</button>
-            </div>
+class InfoVmario extends Component {
+  state =  {
+    renderedResponse: ''
+  }
+
+  getResponse = async() => {
+    const response = await fetch("/api/hello");
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    
+    return body;
+  };
+
+  componentDidMount() {
+    this.getResponse()
+    .then(res =>{
+      const someData = res;
+      this.setState({renderedResponse: someData})
+    })
+  }
+
+
+  render(){ 
+
+    const { renderedResponse } = this.state;
+
+    return( <div className="info-vmario-content">
+    <div className="container-content-vmario">
+      <div className="name-heading">
+        <h1>vMario</h1>
+        <h1></h1>
+      </div>
+      <div className="subs-count">
+        <h4>Ya somos</h4>
+        <div className="subs-number">
+          <div className="num-container">
+            <h2 className="yt-count">{renderedResponse.express}</h2>
           </div>
         </div>
       </div>
     </div>
-  );
-}
-
-function requestData(){
-
-  console.log("button clicked")
-
-  let xhr = new XMLHttpRequest();
-  xhr.open('POST', '/', true)
-
-  xhr.onload=function(){
-    if(this.status == 200){
-      let data = JSON.parse(this.responseText)
-      console.log(data)
-    }else{
-      console.log("aaaa no funca")
-    }
-  }
-
-  xhr.send();
+  </div>
+)}
 }
 
 export default InfoVmario;
